1627356bc222123bf25c3005e4ff1233980c877e
mt-xing <mt-xing@users.noreply.github.com>
2021-12-31 21:05:17 -0500

    Game updates

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
-// @ts-check
-
+import Role from './role';
 import State from './state';
 
 export default class OnuwGame {
@@ -7,4 +6,24 @@ export default class OnuwGame {
 	 * @type {State}
 	 */
 	state;
+
+	/**
+	 * @param {Role[]} roles
+	 * @param {string[]} names
+	 */
+	constructor(roles, names) {
+		this.state = new State(roles, names);
+	}
+
+	/**
+	 * Async factory method
+	 * @param {() => Promise<string[]>} getNames
+	 * @param {() => Promise<Role[]>} getRoles
+	 * @returns {Promise<OnuwGame>}
+	 */
+	static async newGame(getNames, getRoles) {
+		const names = await getNames();
+		const roles = await getRoles();
+		return new OnuwGame(roles, names);
+	}
 }
